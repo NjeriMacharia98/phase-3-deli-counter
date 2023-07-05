e004422b81de0f8c123367a59678d1a0f1e74b2c
Njerimacharia98 <njerimacharia05@gmail.com>
2023-07-05 21:56:04 +0300

    completed lab

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,27 @@
 # Write your code here.
+def line(deli)
+  if deli.empty?
+    puts "The line is currently empty."
+  else
+    status = "The line is currently:"
+    deli.each.with_index(1) do |person, index|
+      status << " #{index}. #{person}"
+    end
+    puts status
+  end
+end
+
+def take_a_number(deli, person)
+  deli << person
+  position = deli.length
+  puts "Welcome, #{person}. You are number #{position} in line."
+end
+
+def now_serving(deli)
+  if deli.empty?
+    puts "There is nobody waiting to be served!"
+  else
+    serving = deli.shift
+    puts "Currently serving #{serving}."
+  end
+end
